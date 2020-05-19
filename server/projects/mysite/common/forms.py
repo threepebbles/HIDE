81e3aa90a44bc0130e37fb12abe05adfe164f532
heiler <threepebbles@ajou.ac.kr>
2020-05-19 16:19:25 +0900

    hot fix

--- server/projects/mysite/common/forms.py
+++ server/projects/mysite/common/forms.py
@@ -4,3 +4,6 @@ from django.contrib.auth.forms import UserCreationForm
 
 class UserForm(UserCreationForm):
     email = forms.EmailField(label="이메일")
+
+class LoginSuccessForm(UserCreationForm):
+    login_state = forms.Field(label="login_state")
--- server/projects/mysite/common/views.py
+++ server/projects/mysite/common/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
-from common.forms import UserForm
+from common.forms import UserForm, LoginSuccessForm
 
 
 def signup(request):
@@ -9,13 +9,16 @@ def signup(request):
     """
     if request.method == "POST":
         form = UserForm(request.POST)
+
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
             login(request, user)
-            return render(request, 'HIDE/question_list.html', {'login_state':'success'})
+
+
+            return render(request, 'HIDE/question_list.html', LoginSuccessForm(request.POST))
     else:
         form = UserForm()
     return render(request, 'common/signup.html', {'form': form})

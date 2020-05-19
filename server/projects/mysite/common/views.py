9a10a689046755a2e876039152a905391e2b16c3
heiler <threepebbles@ajou.ac.kr>
2020-05-19 15:59:27 +0900

    login success comment is added

--- server/projects/mysite/common/views.py
+++ server/projects/mysite/common/views.py
@@ -15,7 +15,7 @@ def signup(request):
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
             login(request, user)
-            return redirect('index')
+            return render(request, 'index', {'login_state':'success'})
     else:
         form = UserForm()
     return render(request, 'common/signup.html', {'form': form})

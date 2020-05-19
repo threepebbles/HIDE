12a7ff874e78df2d1d8a1bb93ba9e0d17854a05e
heiler <threepebbles@ajou.ac.kr>
2020-05-19 16:06:26 +0900

    hot fix

--- server/projects/mysite/common/views.py
+++ server/projects/mysite/common/views.py
@@ -15,7 +15,7 @@ def signup(request):
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
             login(request, user)
-            return render(request, 'HIDE', {'login_state':'success'})
+            return render(request, 'HIDE/question_list.html', {'login_state':'success'})
     else:
         form = UserForm()
     return render(request, 'common/signup.html', {'form': form})

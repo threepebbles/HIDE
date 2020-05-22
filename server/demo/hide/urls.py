828434a3b9f6cc3fde4f996a5580db239d1631a8
heiler <threepebbles@ajou.ac.kr>
2020-05-22 22:59:19 +0900

    rest create, delete added

--- server/demo/hide/urls.py
+++ server/demo/hide/urls.py
@@ -6,6 +6,7 @@ from django.views.generic import TemplateView
 from rest_framework import routers
 
 from .views import base_views, myfile_views
+from .views import myfile_rest_views
 
 app_name = 'hide'
 
@@ -22,4 +23,8 @@ urlpatterns = [
     path('myfile/create/', myfile_views.myfile_create, name='myfile_create'),
     path('myfile/modify/<int:current_author_id>/<int:myfile_index>/', myfile_views.myfile_modify, name='myfile_modify'),
     path('myfile/delete/<int:current_author_id>/', myfile_views.myfile_delete, name='myfile_delete'),
+
+    # myfile_rest_views.py
+    path('myfile/rest/create/', myfile_rest_views.myfile_rest_create, name='myfile_rest_create'),
+    path('myfile/rest/delete/', myfile_rest_views.myfile_rest_delete, name='myfile_rest_delete'),
 ]

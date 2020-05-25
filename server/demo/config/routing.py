cb5961b5d9124d0194978c5e7e6766157e82478c
heiler <threepebbles@naver.com>
2020-05-26 03:10:31 +0900

    127.0.0.1 -> localhost

--- server/demo/config/routing.py
+++ server/demo/config/routing.py
@@ -2,6 +2,8 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 import chat.routing
 
+# ASGI_APPLICATION = "config.routing.application"
+
 application = ProtocolTypeRouter({
     # (http->django views is added by default)
     'websocket': AuthMiddlewareStack(

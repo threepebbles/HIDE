353b5cdf312f762430aaf2fe3c64b2c0bdd4a406
unknown <psm345601@naver.com>
2020-06-10 13:23:16 +0900

    make link dir

--- HIDE UI ver/modules/StateManagement.py
+++ HIDE UI ver/modules/StateManagement.py
@@ -8,6 +8,9 @@ _LINK_DIRECTORY = os.path.abspath(os.path.join(os.path.join('C:/Users/' + os.get
 
 
 def get_path_of_hiddenlist():
+    if not os.path.isdir(os.path.abspath(os.path.join(os.path.join('C:/Users/' + os.getenv('USERNAME')), 'Links/link.{59031a47-3f72-44a7-89c5-5595fe6b30ee}'))):
+        os.mkdir(os.path.abspath(os.path.join(os.path.join('C:/Users/' + os.getenv('USERNAME')), 'Links/link.{59031a47-3f72-44a7-89c5-5595fe6b30ee}')))
+
     f = os.path.abspath(os.path.join(_LINK_DIRECTORY, hashname('C:/' + os.getenv('USERNAME') + 'hidden entry')))
     if not os.path.isfile(f):
         t = open(f, 'w')
